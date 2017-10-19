528ad07abe9799cc549ef4fa7eba1adcda40dca2
sergiroca <sergi_granada1@hotmail.com>
2017-10-19 12:40:52 +0200

    Vuelta al setup inicial

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskapp:sawwexux@myflaskapp.cfgmbp9mytsp.eu-west-2.rds.amazonaws.com:3306/flaskdb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://plantondemand:Fumies9933@plantondemand.cdbbfmyitjua.eu-west-2.rds.amazonaws.com:3306/agricultores' 
 
 # Uncomment the line below if you want to work with a local DB
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///Test1.db'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = 'q7xsaGX1vwEYfFRV+GTuZP1ISrE8JL7QlkoIAvVe'
