9ae7ab03e5cc2a34da313bafbfeae21ecaaf952a
ulisesvidal <ulisesvidalsanz@gmail.com>
2017-10-22 15:46:44 +0200

    Repositorie for test (with AWS for ulises@plantondemand.com credentials)

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://plantondemand:Fumies9933@plantondemand.cdbbfmyitjua.eu-west-2.rds.amazonaws.com:3306/agricultores'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ulises:fumies9933@backtest.cfgmbp9mytsp.eu-west-2.rds.amazonaws.com:3306/agricultores'
 
 # Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///Test1.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'q7xsaGX1vwEYfFRV+GTuZP1ISrE8JL7QlkoIAvVe'
+SECRET_KEY = 'qWWpLZHEI3JufhPSVintw45i1OZxjjGJ7SMllUfo'
