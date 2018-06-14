da592aa9ff6c9a054826498506db666ff88f1fad
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2018-06-14 17:59:42 +0200

    Templates: changed directory from /application/templates to /templates

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 #from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
-application = Flask(__name__, template_folder='templates')
+application = Flask(__name__, template_folder='../templates')
 application.config.from_object('config')
 db = SQLAlchemy(application)
