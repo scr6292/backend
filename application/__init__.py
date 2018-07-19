818f702cc51ab353ce759bc863afbb56734cd17f
sergiroca <sergi_granada1@hotmail.com>
2018-07-19 16:17:13 +0200

    Add static url path

--- application/__init__.py
+++ application/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__, template_folder='../templates')
+application = Flask(__name__, static_url_path='../application/static')
+
 application.config.from_object('config')
 db = SQLAlchemy(application)
